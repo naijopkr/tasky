bc8970ca34ba33e75d3b17d1215d1c4e2520b632
Ariel Barcellos <barcellos.ariel@gmail.com>
2019-01-21 19:41:23 -0500

    initial commit (from boilerplate)

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var webpack = require('webpack');
+var path = require('path');
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+module.exports = {
+  externals: nodeModules,
+  entry: [
+    './src/index.js'
+  ],
+  target: 'node',
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    port: 4172
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      }
+    ]
+  }
+};
