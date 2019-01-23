9d95b774f1efd646fee405d2887a6dafa4f91624
Ariel Barcellos <barcellos.ariel@gmail.com>
2019-01-22 19:25:04 -0500

    configured the app main window

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const { app, BrowserWindow } = require('electron')
 let mainWindow
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({})
+  mainWindow = new BrowserWindow({
+    width: 300,
+    height: 500,
+    frame: false,
+    resizable: false
+  })
   mainWindow.loadURL(`file://${__dirname}/src/index.html`)
 })
