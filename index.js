cecf47b2115db4f377919f065207fb10b72f829d
Ariel Barcellos <barcellos.ariel@gmail.com>
2019-01-22 19:39:29 -0500

    implemented toggle window visibility feature

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ const path = require('path')
 const { app, BrowserWindow, Tray } = require('electron')
 
 let mainWindow
+let tray
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
     width: 300,
     height: 500,
     frame: false,
-    resizable: false
+    resizable: false,
+    show: false
   })
   mainWindow.loadURL(`file://${__dirname}/src/index.html`)
 
   const iconName = process.platform === 'darwin' ? 'iconTemplate.png' : 'windows-icon.png'
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`)
-  new Tray(iconPath)
+  tray = new Tray(iconPath)
+  tray.on('click', () => {
+    if (mainWindow.isVisible()) {
+      mainWindow.hide()
+    } else {
+      mainWindow.show()
+    }
+  })
 })
